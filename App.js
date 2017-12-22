ddf7aedd5a5619197dec2af546a2a90f8e698c14
Fredrik Paasche <fredrik.paasche@gmail.com>
2017-12-22 12:10:10 +0100

    Added crash and analytics via cocoapods

--- App.js
+++ App.js
@@ -1,57 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Analytics from 'appcenter-analytics';
+import Crashes from 'appcenter-crashes';
 
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.big}>
+                    App Center demo
+                </Text>
+                <Button
+                    onPress={() => Analytics.trackEvent('Button tapped', { date: new Date().toString()})}
+                    title="Tap to track an event"
+                />
+                <Button
+                    onPress={() => Crashes.generateTestCrash()}
+                    title="Tap to crash the app"
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    big: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
 });
---
+++ ios/Pods/Target Support Files/Pods-AppCenterExample-tvOS/Pods-AppCenterExample-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppCenterExample_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_AppCenterExample_tvOS
+@end
